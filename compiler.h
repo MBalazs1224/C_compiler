f00682fb0313f05b82d1d277e9327b1b9c8e6452
FIXED-TERM Molnar Balazs (HtvP/MFD-NPI) <mob2htv@bosch.com>
2024-02-13 14:14:51 +0100

    Continued struct and created the base for bodys

--- compiler.h
+++ compiler.h
@@ -352,6 +352,23 @@ struct node
              */
             struct node* var;
         }_struct;
+
+
+        struct body
+        {
+            // struct node* elements
+            struct vector* statements;
+
+            // sum of all variable declarations inside the body
+            size_t size;
+
+            // true if the variable sized had do be increased due to padding in the body
+            bool padded;
+
+            //pointer to the largest variable in the vector
+            struct node* largest_var_node;
+
+        } body;
     };
     
 
@@ -452,6 +469,7 @@ bool token_is_operator(struct token* token, const char* val);
 struct node* node_create(struct node* _node);
 void make_exp_node(struct node* left_node, struct node* right_node, const char* op);
 void make_bracket_node(struct node* node);
+void make_body_node(struct vector* body_vec,size_t size, bool padded, struct node* largest_var_node);
 bool keyword_is_datatype(const char *str);
 
 struct node* node_pop();
